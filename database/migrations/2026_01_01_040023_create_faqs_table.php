d8a2b2f3c5436f08055cdccdb2d93397681d26d2
habibursoftvence <habibur@softvencedelta>
2026-03-25 11:10:28 +0600

    update

--- database/migrations/2026_01_01_040023_create_faqs_table.php
+++ database/migrations/2026_01_01_040023_create_faqs_table.php
@@ -6,14 +6,11 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    
+
     public function up(): void
     {
         Schema::create('faqs', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('faq_category_id')
-                ->constrained('faq_categories')
-                ->cascadeOnDelete();
             $table->string('question');
             $table->text('answer');
             $table->boolean('status')->default(true);
