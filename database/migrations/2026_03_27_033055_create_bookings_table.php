65fc3ab1317b956f87ea0d0de009734309adc58e
habibursoftvence <habibur@softvencedelta>
2026-03-27 10:40:23 +0600

    u

--- database/migrations/2026_03_27_033055_create_bookings_table.php
+++ database/migrations/2026_03_27_033055_create_bookings_table.php
@@ -36,7 +36,7 @@ return new class extends Migration
             $table->string('question_four')->nullable();
             $table->string('question_five')->nullable();
             $table->string('question_six')->nullable();
-            $table->string('booking_code')->unique()->nullable();
+            $table->string('include_booking_id')->unique()->nullable();
             $table->boolean('agree_terms')->default(false);
             $table->enum('payment_method', ['stripe'])->nullable();
             $table->enum('status', ['pending', 'confirmed', 'completed', 'cancelled'])->default('pending');
