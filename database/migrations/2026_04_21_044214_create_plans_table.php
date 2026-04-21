14d6c65cdd9535ed33e04a8b2d633b13d1a96df3
Habibur5114 <habiburrahman5114@gmail.com>
2026-04-21 11:05:40 +0600

    update

---
+++ database/migrations/2026_04_21_044214_create_plans_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('plans', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->nullable();
+            $table->string('title')->nullable();
+            $table->decimal('price', 10, 2)->default(0);
+            $table->string('currency', 10)->default('USD');
+            $table->string('package')->nullable();
+            $table->integer('day')->default(0); // plan validity
+            $table->text('features')->nullable();
+            $table->tinyInteger('status')->default(1); // 1 = active, 0 = inactive
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('plans');
+    }
+};
---
+++ database/migrations/2026_04_21_045203_create_subscriptions_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('subscriptions', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('plan_id')->constrained()->onDelete('cascade');
+            $table->string('starts_at')->nullable();
+            $table->string('ends_at')->nullable();
+            $table->integer('status')->default(1);
+            $table->timestamp('auto_renewal')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('subscriptions');
+    }
+};
