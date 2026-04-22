24040acd2a57284e44f6ffc7812da7f6e19f3867
Habibur5114 <habiburrahman5114@gmail.com>
2026-04-22 17:38:47 +0600

    update

--- routes/api.php
+++ routes/api.php
@@ -158,11 +158,6 @@ Route::middleware(['auth:api'])->prefix('admin')->name('admin.')->group(function
         Route::get('index', [MessageController::class, 'index']);
         Route::post('send', [MessageController::class, 'sendMessage']);
     });
-    // Route::prefix('message')->group(function () {
-    //     Route::get('index', [MessageController::class, 'index']);
-    //     Route::post('send', [MessageController::class, 'sendMessage']);
-
-    // });
 
     //Order
     Route::prefix('order')->group(function () {
