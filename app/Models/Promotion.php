9d37c3d4bd3153dde8d170eee78b3f03ae62780a
Habibur5114 <habiburrahman5114@gmail.com>
2026-05-19 08:43:02 +0600

    update

--- app/Models/Promotion.php
+++ app/Models/Promotion.php
@@ -15,13 +15,5 @@ class Promotion extends Model
         'status',
 
     ];
-    // protected $fillable = [
-    //     'name',
-    //     'discount',
-    //     'type',
-    //     'start_date',
-    //     'end_date',
-    //     'status',
-
-    // ];
+    
 }
