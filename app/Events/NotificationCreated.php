e738a354e4b512c258c62af8333d6857a519252b
Habibur5114 <habiburrahman5114@gmail.com>
2026-06-12 10:18:34 +0600

    update

--- app/Events/NotificationCreated.php
+++ app/Events/NotificationCreated.php
@@ -28,8 +28,5 @@ class NotificationCreated implements ShouldBroadcast
         return 'notification.created';
     }
 
-    // public function broadcastAs(): string
-    // {
-    //     return 'notification.created';
-    // }
+   
 }
